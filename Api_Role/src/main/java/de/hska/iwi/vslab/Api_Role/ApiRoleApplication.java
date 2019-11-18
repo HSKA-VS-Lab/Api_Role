0ca049cc94cfb98822492370ee915b90d07f6a82
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-18 21:18:48 +0100

    Initial Commit

---
+++ Api_Role/src/main/java/de/hska/iwi/vslab/Api_Role/ApiRoleApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Api_Role;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRoleApplication.class, args);
+	}
+
+}
---
+++ Api_Role/src/test/java/de/hska/iwi/vslab/Api_Role/ApiRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Api_Role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
